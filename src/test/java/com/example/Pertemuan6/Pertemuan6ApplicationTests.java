bc3faaa53224ab1633556dcfb750a77ffd6c58ce
jihaduttolibinn <jihaduttolibin02@gmail.com>
2026-04-13 13:39:49 +0700

    Initial commit

---
+++ src/test/java/com/example/Pertemuan6/Pertemuan6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pertemuan6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pertemuan6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
